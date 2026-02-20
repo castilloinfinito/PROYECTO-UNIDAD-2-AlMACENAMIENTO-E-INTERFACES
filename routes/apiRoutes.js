9458c17287e31114e55d11cd9c1303bd1831c7ae
Maria Vargas uvm <vargastorresmal@uvm.edu.ve>
2026-02-20 16:51:50 -0400

    comentado de los bloques logicos mas importantes del enrutador, verificacion de funcionamiento.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,9 @@
+//importacion de los modulos requeridos para el enrutamiento dinamico
 const express = require('express');
 const Ctrl = require('../controllers/GenericController');
 const router = express.Router();
 
+// Ctrl:Importas un objeto que contiene toda la lógica de control (los controladores)
 const mapeo = {
   pacientes: Ctrl.PacienteCtrl,
   medicos: Ctrl.MedicoCtrl,
@@ -9,12 +11,12 @@ const mapeo = {
   usuarios: Ctrl.UsuarioCtrl,
   resultados: Ctrl.ResultadoCtrl
 };
-
+// El Bucle Dinámico (forEach).
 Object.keys(mapeo).forEach(ent => {
   router.get(`/${ent}`, mapeo[ent].listar);
   router.post(`/${ent}`, mapeo[ent].crear);
   router.put(`/${ent}/:id`, mapeo[ent].actualizar);
   router.delete(`/${ent}/:id`, mapeo[ent].eliminar);
 });
-
+// exportar el paquete de enrutador
 module.exports = router;
